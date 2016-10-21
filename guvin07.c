6db130352e8d2e65c52d0eb1c66df759521b6582
Tamil1997 <Tamil1997@users.noreply.github.com>
2016-10-22 01:11:25 +0530

    Add files via upload

---
+++ guvin07.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n,a;
+printf("\n enter the number");
+scanf("%d",&n);
+a=(n*(n+1))/2;
+printf("\n the sum of natural number is %d",a);
+getch();
+}
