2993b5ae284e9ec14542a7fd4fc6a05b5ac629b6
Tamil1997 <Tamil1997@users.noreply.github.com>
2016-10-21 17:53:43 +0530

    Add files via upload

---
+++ guvin02.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char input;
+printf("\n enter the input");
+scanf("%c",&input);
+if(input=='a')
+printf("\n the given input is vowel");
+else if(input=='e')
+printf("\n the given input is vowel");
+else if(input=='i')
+printf("\n the given input is vowel");
+else if(input=='o')
+printf("\n the given input is vowel");
+else if(input=='u')
+printf("\n the given input is vowel");
+else
+printf("\n the given input is not a vowel");
+getch();
+}
