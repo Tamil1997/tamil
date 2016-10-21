57d0827ab1be6f7c83bea32640a0248d3f7f9681
Tamil1997 <Tamil1997@users.noreply.github.com>
2016-10-21 17:30:50 +0530

    Add files via upload

---
+++ guvin01.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int n;
+printf("\n enter the number");
+scanf("%d",&n);
+if(n%2==0)
+printf("\n the given number is even");
+else
+printf("\n the given number is odd");
+getch();
+}
