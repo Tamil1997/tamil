af512d7972e2fc41fc7067efbbb53044aa6d1d93
Tamil1997 <Tamil1997@users.noreply.github.com>
2016-12-27 16:04:23 +0530

    Add files via upload

---
+++ guv5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n;
+	printf("\n enter the no");
+	scanf("%d",&n);
+	printf("the alphabet for the no is %c",n+64);
+	getch();
+
+	return 0;
+	
+}
