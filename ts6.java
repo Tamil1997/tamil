e83d2c60ee8a4cf7f48a38384a27fea298c2ca48
Tamil1997 <Tamil1997@users.noreply.github.com>
2017-03-24 11:34:56 +0530

    Add files via upload

---
+++ ts6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class guvi {
+	public static  void main(String args[]){
+		Scanner n=new Scanner(System.in);
+		int s=n.nextInt();
+		
+		for(int i=0;i<s;i++)
+		{
+			System.out.println("hello");
+		}
+		
+	}
+
+}
